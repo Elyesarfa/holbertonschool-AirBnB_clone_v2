e1a96940311046fe14c042c1c9acc239d2ad14c5
Elyesrafa <elyesarfa7211@gmail.com>
2024-03-31 23:09:33 +0100

    test

--- models/city.py
+++ models/city.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
 from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
